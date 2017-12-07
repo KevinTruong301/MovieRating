70ee39319ad7e7b09523eeeb278326a099aa7edc
KevinTruong301 <kevintruong30195@gmail.com>
2017-12-07 02:50:49 -0800

    Cancel in editMovie now works

--- app/src/main/java/edu/fullerton/kevin/movierating/EditMovie.java
+++ app/src/main/java/edu/fullerton/kevin/movierating/EditMovie.java
@@ -4,19 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Kevin on 12/7/2017.
  */
 
-public class EditMovie extends AppCompatActivity {
+public class EditMovie extends AppCompatActivity implements View.OnClickListener{
 
     private String name;
     private String date;
     private int rating;
     private boolean askMeLater;
     private Intent intent;
+    private Button save;
+    private Button cancel;
+    private Button delete;
 
+
+    public EditMovie() {
+
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,11 +38,28 @@ public class EditMovie extends AppCompatActivity {
         date = intent.getStringExtra("date");
         rating = intent.getIntExtra("rating", 0);
         askMeLater = intent.getBooleanExtra("askMeLater", false);
+
+        save = (Button) findViewById(R.id.save);
+        save.setOnClickListener(this);
+
+        cancel = (Button) findViewById(R.id.cancel);
+        cancel.setOnClickListener(this);
+
+        delete = (Button) findViewById(R.id.delete);
+        delete.setOnClickListener(this);
+
     }
 
-
-
-    public EditMovie() {
-
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.save:
+                break;
+            case R.id.cancel:
+                Intent intent = new Intent(this, Main.class);
+                startActivity(intent);
+                break;
+            case R.id.delete:
+        }
     }
 }
